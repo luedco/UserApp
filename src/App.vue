973e10115ad313a9c6e13d83a9485f36dbb2c5e1
luedco <luedcomu@gmail.com>
2020-08-12 23:17:13 -0600

    first changes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Estoy probando Vue.JS"/>
+    <MiComponente />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import MiComponente from "./components/MiComponente"
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    MiComponente
+  },
 }
-</script>
 
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(user,index) in users" :key="index+lastName+name">{{user.name}} {{user.lastName}}</li>
+        </ul>
+        <button v-on:click="isHidden = !isHidden">Add Componente</button>
+        <div v-if="!isHidden" >
+            <input v-model="newUserName">
+            <br>
+            <input v-model="newUserlName">
+            <br>
+            <button @click="addUser" class="button" v-on:click="isHidden=true">Icono</button>
+        </div>
+    </div>
+     
+</template>
+
+<script>
+export default {
+    name: "MiComponente",
+    data(){
+        return{
+            isHidden:true,
+            newUserName:"",
+            newUserlName:"",
+            users:[
+                {
+                    name:"Luis",
+                    lastName:"Cortez",
+                    imageUrl:""
+                },
+                {
+                    name:"Eduardo",
+                    lastName:"Murillo",
+                    imageUrl:""
+                }
+                ,{
+                    name:"Vale",
+                    lastName:"Cortez",
+                    imageUrl:""
+                }
+                ]
+        }
+        
+    },
+    methods:{
+        addUser(){
+            if(this.newUserName !== '' || this.newUserlName !==''){
+                this.users.push({
+                    name:this.newUserName,
+                    lastName:this.newUserlName
+                })
+                this.newUserName = ""
+                this.newUserlName = ""
+            }
+        }
+    }
+}
+</script>
+<style>
+.miC{
+    background-color: red;
+}
+
+.users{
+    border-top:1px solid #ddd;
+}
+
+.name{
+    float: left;
+   
+}
+
+.lastname{
+    float: right;
+    
+}
+</style>
